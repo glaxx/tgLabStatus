9409b389b177653e821688e6d9cde0180e6d3849
Stefan Luecke <glaxx@glaxx.net>
2016-08-28 23:25:34 +0200

    Updated copyright information

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-//    Copyright (C) 2015 Stefan Luecke
+//    Copyright (C) 2016 Stefan Luecke
 //
 //    This program is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU Affero General Public License as published
